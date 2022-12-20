35160297c31debd7164a7c9d6dc831a88fa78f55
David Saarikari <david.saarikari@gmail.com>
2022-12-20 17:12:34 +0100

    Graphs graphs!

--- app/src/main/java/nu/veberod/healthmonitor/presentation/MainActivity.kt
+++ app/src/main/java/nu/veberod/healthmonitor/presentation/MainActivity.kt
@@ -4,38 +4,36 @@
  * changes to the libraries and their usages.
  */
 
-// David is here
-
-
 package nu.veberod.healthmonitor.presentation
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import nu.veberod.healthmonitor.R
 import nu.veberod.healthmonitor.presentation.theme.HealthMonitorTheme
 
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp("Android")
+           WearApp("Android")
+
         }
     }
 }
+
 
 @Composable
 fun WearApp(greetingName: String) {
@@ -70,3 +68,4 @@ fun Greeting(greetingName: String) {
 fun DefaultPreview() {
     WearApp("Preview Android")
 }
+
